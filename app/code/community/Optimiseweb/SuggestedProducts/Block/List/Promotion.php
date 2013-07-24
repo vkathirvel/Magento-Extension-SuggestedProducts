a8352632f1f0447e920d9c4d635a5822589a720c
Kathirvel <sid@kathirvel.com>
2013-07-24 16:28:33 +0100

    Update

--- app/code/community/Optimiseweb/SuggestedProducts/Block/List/Promotion.php
+++ app/code/community/Optimiseweb/SuggestedProducts/Block/List/Promotion.php
@@ -5,7 +5,7 @@
  *
  * @package     Optimiseweb_SuggestedProducts
  * @author      Kathir Vel (sid@optimiseweb.co.uk)
- * @copyright   Copyright (c) 2013 Optimise Web Ltd
+ * @copyright   Copyright (c) 2013 Optimise Web Limited
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Optimiseweb_SuggestedProducts_Block_List_Promotion extends Mage_Catalog_Block_Product_List
